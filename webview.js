0342cd8a537e365895c00118f255cebeac71f41f
Dominik Sander <git@dsander.de>
2019-02-23 11:09:21 +0100

    Fix notification bubble when being in multiple teams
    It is possible to join multiple teams on the same Mattermost server.
    Once one joins another team, Mattermost will show add a team sidebar.
    When the user selected a different team, Mattermost shows the
    notifications for other teams in this sidebar. To propagate the counter
    to Franz we also need to extract the number of notifications for
    different teams.

--- webview.js
+++ webview.js
@@ -2,8 +2,10 @@ module.exports = (Franz) => {
   const getMessages = function getMessages() {
     const directMessages = document.querySelectorAll('.sidebar--left .has-badge .badge').length;
     const allMessages = document.querySelectorAll('.sidebar--left .has-badge').length - directMessages;
+    const teamDirectMessages = document.querySelectorAll('.team-wrapper .team-container .badge').length;
+    const teamMessages = document.querySelectorAll('.team-wrapper .unread').length - teamDirectMessages;
 
-    Franz.setBadge(directMessages, allMessages);
+    Franz.setBadge(directMessages + teamDirectMessages, allMessages + teamMessages);
   };
 
   Franz.loop(getMessages);
